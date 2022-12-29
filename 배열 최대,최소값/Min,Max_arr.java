937a6b2b174d89f0b84898cf9edf7fd748a390eb
PARK GEONDO <116621655+GD000908@users.noreply.github.com>
2022-12-29 21:35:27 +0900

    Create Min,Max_arr.java

---
+++ 배열 최대,최소값/Min,Max_arr.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+        
+		int N = in.nextInt();
+		int[] arr = new int[N];
+        
+		for (int i = 0; i < N; i++) {
+			arr[i] = in.nextInt();
+		}
+        
+		in.close();
+		Arrays.sort(arr); //배열을 오름차순으로 정렬해준다. 
+		System.out.print(arr[0] + " " + arr[N - 1]); //오름차순이므로 arr[0]은 최대값. arr[N-1]은 최소값
+	}
+}
